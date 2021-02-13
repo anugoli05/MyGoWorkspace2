13f40892b8bd882c907be95a9dc550f1a8a999a6
Chandana Goli <cgoli@nthrive.com>
2021-02-12 21:12:24 -0600

    Mypackages as part of repository

--- Subtract/Sub.go
+++ Subtract/Sub.go
@@ -1,11 +1,11 @@
 package subtract
 
 import (
-	"github.com/anugoli05/MyGoPackages/mathutils"
+	"github.com/anugoli05/MyGoWorkspace2/MyPackages/mathutils"
 )
 //CallSub is a function to call subtract function of the mygopackage to maintain my own package
 func CallSub(a, b int) int{
 var Subresult int
-Subresult=	mathutils.Subtractt(a,b)
+Subresult=	mathutils.Subtract(a,b)
 return Subresult
 }
